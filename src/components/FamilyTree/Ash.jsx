195ba088727c85a47928f2347da034b2b6206157
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-16 23:14:26 +0600

    Context API explored

--- src/components/FamilyTree/Ash.jsx
+++ src/components/FamilyTree/Ash.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
 const Ash = ({asset}) => {
+    const newAsset = useContext(AssetContext);
     return (
         <div>
             <h5>Ashfa</h5>
             <p>Asset: {asset}</p>
+            <p>New Asset: {newAsset}</p>
         </div>
     );
 };
--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Dadu from './Dadu';
 import './FamilyTree.css'
 
+export const AssetContext = createContext('');
+
 const FamilyTree = () => {
     const asset = 'Diamond';
+    const NewAsset = 'Gold';
     return (
         <div className='family_tree'>
-            <Dadu asset={asset}></Dadu>
+            <AssetContext.Provider value={NewAsset}>
+                <Dadu asset={asset}></Dadu>
+            </AssetContext.Provider>
         </div>
     );
 };
